16c489b8a9901c38a99152274d31133e2650e6c0
Joseph <joseph.leon@tecsup.edu.pe>
2024-07-25 16:39:26 -0500

    se agrego UseAxios.jsx

---
+++ src/hooks/UseAxios.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useAxios = (url, options = {}) => {
+    const [data , setData] = useState(null);
+    const [error , setError] = useState(null);
+    const [loading , setLoading] = useState(true);
+
+    useEffect(() =>{
+        const fetchData = async() {
+            try {
+                const response = await axios(url, options);
+                setData(response.data)
+                setError(null);
+
+            } catch (err) {
+                setData(null);
+                setError(err);
+                
+            } finally{
+                setLoading(false);
+
+            }
+        };
+        fetchData();    
+    }, [url, options]);
+    
+return {data, options, loading}
+};
+
+export default useAxios;
