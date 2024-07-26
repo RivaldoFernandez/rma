1e561831265b4c9993bac9ee7636839a5e480a80
rivaldofernandez <roman.qquelcca@tecsup.edu.pe>
2024-07-26 08:33:41 -0500

    Comento componente useAxios

--- src/hooks/UseAxios.jsx
+++ src/hooks/UseAxios.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
-const useAxios = (url, options = {}) => {
-    const [data , setData] = useState(null);
-    const [error , setError] = useState(null);
-    const [loading , setLoading] = useState(true);
+// const useAxios = (url, options = {}) => {
+//     const [data , setData] = useState(null);
+//     const [error , setError] = useState(null);
+//     const [loading , setLoading] = useState(true);
 
-    useEffect(() =>{
-        const fetchData = async() {
-            try {
-                const response = await axios(url, options);
-                setData(response.data)
-                setError(null);
+//     useEffect(() =>{
+//         const fetchData = async() {
+//             try {
+//                 const response = await axios(url, options);
+//                 setData(response.data)
+//                 setError(null);
 
-            } catch (err) {
-                setData(null);
-                setError(err);
+//             } catch (err) {
+//                 setData(null);
+//                 setError(err);
                 
-            } finally{
-                setLoading(false);
+//             } finally{
+//                 setLoading(false);
 
-            }
-        };
-        fetchData();    
-    }, [url, options]);
+//             }
+//         };
+//         fetchData();    
+//     }, [url, options]);
     
-return {data, options, loading}
-};
+// return {data, options, loading}
+// };
 
-export default useAxios;
+// export default useAxios;
--- src/pages/Monitoreo.jsx
+++ src/pages/Monitoreo.jsx
@@ -19,11 +19,11 @@ import { FaHome, FaChartLine, FaHistory, FaCloudSun, FaMapSigns, FaClipboardList
 
 
 import '../style/Monitoreo.css';
-import useAxios from '../hooks/UseAxios';
+//import useAxios from '../hooks/UseAxios';
 const Monitoreo = () => {
-    const {data, error, loading} = useAxios( 'https://3p7jzhtc-8000.brs.devtunnels.ms/api/lecturasRaspberry/ultima/1/');
-    if (loading) return <p>cargando</p>;
-    if (error) return  <p>error {error.message}</p>;
+    // const {data, error, loading} = useAxios( 'https://3p7jzhtc-8000.brs.devtunnels.ms/api/lecturasRaspberry/ultima/1/');
+    // if (loading) return <p>cargando</p>;
+    // if (error) return  <p>error {error.message}</p>;
     
 return (
 <div className="monitoreo-container-2">
